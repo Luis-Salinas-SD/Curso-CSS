9211cad1e3c052dca140e65544e0038196ce1932
Luis-Salinas-SD <paus.impala21@gmail.com>
2020-07-15 15:42:40 -0500

    Primer-commit

---
+++ ind.js
@@ -0,0 +1,9 @@
+var mensaje = "gana global";
+function imprimirmensaje() {
+    var mensaje = "gana loca";
+    console.log(mensaje);
+}
+
+console.log(mensaje);
+imprimirmensaje();
+console.log(mensaje);
